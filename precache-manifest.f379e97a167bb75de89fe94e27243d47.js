0553c02bb981b27be01d3dbe1969c3356bf4f66d
Aldo Gomez <gomezfx@gmail.com>
2019-01-24 19:11:38 -0800

    Updates

---
+++ precache-manifest.f379e97a167bb75de89fe94e27243d47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "468a9fd8b9581dcd646c",
+    "url": "/nazuks-site/static/css/main.b60f996b.chunk.css"
+  },
+  {
+    "revision": "468a9fd8b9581dcd646c",
+    "url": "/nazuks-site/static/js/main.468a9fd8.chunk.js"
+  },
+  {
+    "revision": "e607849d5aa2061fc081",
+    "url": "/nazuks-site/static/js/1.e607849d.chunk.js"
+  },
+  {
+    "revision": "1fe027a4bcca126af481",
+    "url": "/nazuks-site/static/js/runtime~main.1fe027a4.js"
+  },
+  {
+    "revision": "0afbafb309f127b2226ab8bbe60e9dc5",
+    "url": "/nazuks-site/static/media/DrukWide-Medium.0afbafb3.otf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nazuks-site/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c4edef01c56d5dc8940e7a1344b6e842",
+    "url": "/nazuks-site/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/nazuks-site/precache-manifest.f379e97a167bb75de89fe94e27243d47.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/nazuks-site/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
