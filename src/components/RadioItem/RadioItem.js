50fec1bd831fe9ce722c4188cd4acc666e406dcb
Aldo Gomez <gomezfx@gmail.com>
2019-04-17 18:34:23 -0700

    added radio component

---
+++ src/components/RadioItem/RadioItem.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+
+const StyledRadioItem = styled.li`
+`;
+
+class RadioItem extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <StyledRadioItem>
+
+      </StyledRadioItem>
+    );
+  }
+}
+
+export default SocialLink
